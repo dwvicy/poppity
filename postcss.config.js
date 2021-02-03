31a691364e36abdf7839a59b264ec613a73d07f2
Vaishnavi <vaishdw2207@gmail.com>
2021-02-03 13:43:05 +0530

    fix: postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,18 @@
 module.exports = {
   plugins: [
     'tailwindcss',
+    'postcss-flexbugs-fixes',
     [
-      '@fullhuman/postcss-purgecss',
-      process.env.NODE_ENV === 'production'
-        ? {
-            // the paths to all template files
-            content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}'
-            ],
-            // function used to extract class names from the templates
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || []
-          }
-        : false
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009'
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false
+        }
+      }
     ]
   ]
 }
