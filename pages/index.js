4b98ec2726f757bbab334368674e8299b8cad56a
Vaishnavi <vaishdw2207@gmail.com>
2021-02-06 17:02:12 +0530

    feat: almost done with landing

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from 'next/head';
+import Foto from '../src/components/foto';
 
 export default function Home() {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen items-center">
             <Head>
                 <title>dwvicy</title>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="../public/ww-logo.png" />
             </Head>
+
+            <Foto />
+            <h1 className="text-2xl font-light py-2">Vaishnavi Dwivedi</h1>
+            <p>Full Stack Developer | Data Engineer | Designer</p>
         </div>
     );
 }
