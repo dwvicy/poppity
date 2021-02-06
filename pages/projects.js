4b1cdba7ec8859f6b6eb87c98cda58431cd79af1
Vaishnavi <vaishdw2207@gmail.com>
2021-02-06 18:49:18 +0530

    test: project card

--- pages/projects.js
+++ pages/projects.js
@@ -1,20 +1,22 @@
 import React from 'react';
-// import ProjectCard from '../src/components/project-card';
+import ProjectCard from '../src/components/project-card';
 
 export default function Projects() {
     return (
         <div className="flex flex-col min-h-screen">
             <h1>Projects</h1>
-            {/* <ProjectCard
+            <ProjectCard
+                img="https://i.imgur.com/4IHOCRG.png"
                 title="Niyamo"
                 subtitle="An app to overcome imposter syndrome"
                 label="flutter"
             />
             <ProjectCard
+                img="https://i.imgur.com/4IHOCRG.png"
                 title="CognitionAD"
                 subtitle="An app for early detection of Alzheimer's disease"
                 label="flutter"
-            /> */}
+            />
         </div>
     );
 }
