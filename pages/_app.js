226fe4a6157b9f3278e3d8af86f69ceb5b38765f
Vaishnavi <vaishdw2207@gmail.com>
2021-02-06 19:24:41 +0530

    hypothesis

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import Foot from '../src/components/foot';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <div className="flex flex-col h-screen">
+        <div className="flex h-screen">
             <Nav />
             <main className="flex-1 overflow-y-auto p-5">
                 <Component {...pageProps} />
