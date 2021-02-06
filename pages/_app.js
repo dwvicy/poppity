f9d938f8038f19e5138ae7d739609874ce25a68e
Vaishnavi <vaishdw2207@gmail.com>
2021-02-06 18:46:56 +0530

    fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import '../styles/tailwind.scss';
+import '../styles/globals.css';
+import '../styles/tailwind.css';
 import Nav from '../src/components/nav';
 import Foot from '../src/components/foot';
 
--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,16 @@ export default function Home() {
                 <link rel="icon" href="../public/ww-logo.png" />
             </Head>
 
-            <div className="container mx-auto w-10/12 sm:w-4/12 px-4 align-middle">
+            <div className="flex flex-wrap w-10/12 sm:w-4/12 px-4 justify-center">
                 <img
                     src="https://i.imgur.com/NMVIzOW.png"
                     alt="..."
                     className="shadow-lg rounded-full align-middle border-none"
                 />
-                <h1 className="text-center text-2xl font-light pt-3">Vaishnavi Dwivedi</h1>
-                <br></br>
-                <p className="text-center pb-10">Full Stack Developer | Data Engineer | Designer</p>
             </div>
+            <h1 className="text-center text-2xl font-light pt-3">Vaishnavi Dwivedi</h1>
+            <br></br>
+            <p className="text-center pb-10">Full Stack Developer | Data Engineer | Designer</p>
         </div>
     );
 }
