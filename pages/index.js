e60e9fb96e835435a0137510fbf8e9ae5011f589
Vaishnavi <vaishdw2207@gmail.com>
2021-02-06 18:00:07 +0530

    fix: removed component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
 import Head from 'next/head';
-import Foto from '../src/components/foto';
 
 export default function Home() {
     return (
-        <div
-            style={{ flexBasis: 0, flexGrow: 4 }}
-            className=" flex flex-col min-h-screen items-center">
+        <div className=" flex flex-col min-h-screen items-center justify-center">
             <Head>
                 <title>dwvicy</title>
                 <link rel="icon" href="../public/ww-logo.png" />
             </Head>
 
-            <Foto />
+            <div className="w-10/12 sm:w-5/12 px-4">
+                <img
+                    src="https://i.imgur.com/NMVIzOW.png"
+                    alt="..."
+                    className="shadow-lg rounded-full max-w-full h-auto align-middle border-none"
+                />
+            </div>
             <h1 className="text-2xl font-light py-2">Vaishnavi Dwivedi</h1>
             <p>Full Stack Developer | Data Engineer | Designer</p>
         </div>
