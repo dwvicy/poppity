c3514d95255950c82e169fe599e903fc3d330456
Vaishnavi <vaishdw2207@gmail.com>
2021-02-06 20:24:56 +0530

    fix: postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,17 @@
 module.exports = {
     plugins: [
-        require('tailwindcss'),
-        require('postcss-flexbugs-fixes'),
-        require('autoprefixer')[
-            (require('postcss-preset-env'),
+        'tailwindcss',
+        'postcss-flexbugs-fixes',
+        'autoprefixer',
+        [
+            'postcss-preset-env',
             {
                 autoprefixer: {},
                 stage: 3,
                 features: {
                     'custom-properties': false
                 }
-            })
+            }
         ]
     ]
 };
