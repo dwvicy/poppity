2451d4d24a1c80b351271e4fd527764c23a33c74
Vaishnavi <vaishdw2207@gmail.com>
2021-02-06 20:26:46 +0530

    add flex-box config

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,9 @@ module.exports = {
         [
             'postcss-preset-env',
             {
-                autoprefixer: {},
+                autoprefixer: {
+                    flexbox: 'no-2009'
+                },
                 stage: 3,
                 features: {
                     'custom-properties': false
