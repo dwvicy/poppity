43e72e4beabe0e8e3aea0b5680cc28dea30172d4
Vaishnavi <vaishdw2207@gmail.com>
2021-02-06 19:04:03 +0530

    fix: revert

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,3 @@
-const purgecss = {
-    '@fullhuman/postcss-purgecss': {
-        // Use this if you have `./components` folder
-        // content: ["./components/**/*.js", "./pages/**/*.js"],
-        content: ['./pages/**/*.js', './src/components/**/*.js'],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
-    }
-};
-
 module.exports = {
     plugins: [
         'tailwindcss',
@@ -20,7 +11,6 @@ module.exports = {
                     'custom-properties': false
                 }
             }
-        ],
-        ...(process.env.NODE_ENV === 'production' ? { ...purgecss, cssnano: {} } : {})
+        ]
     ]
 };
