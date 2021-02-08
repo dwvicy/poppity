f790fd2aeb147741aa8c7262c5384b565ecc9f57
Vaishnavi <vaishdw2207@gmail.com>
2021-02-08 13:43:09 +0530

    feat: populate personal projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,43 +1,21 @@
 import React from 'react';
 import ProjectCard from '../src/components/project-card';
+import { projects } from '../src/data/project-data';
 
 export default function Projects() {
     return (
         <div>
-            <div className="w-28">
-                <h1 className="font-black text-2xl underline--magical">Projects</h1>
+            <div className="w-56">
+                <h1 className="font-black text-2xl underline--magical">personal projects</h1>
             </div>
-            <div className="flex flex-col sm:flex-row sm:space-x-4">
-                <ProjectCard
-                    img="https://i.imgur.com/4IHOCRG.png"
-                    title="niyamo"
-                    subtitle="an app to overcome imposter syndrome"
-                    label="flutter"
-                />
-                <ProjectCard
-                    img="https://i.imgur.com/4IHOCRG.png"
-                    title="cognitionAD"
-                    subtitle="an app for early detection of Alzheimer's disease"
-                    label="flutter"
-                />
-                <ProjectCard
-                    img="https://i.imgur.com/4IHOCRG.png"
-                    title="aereor"
-                    subtitle="an app for early detection of Alzheimer's disease"
-                    label="flutter"
-                />
-                <ProjectCard
-                    img="https://i.imgur.com/4IHOCRG.png"
-                    title="michi"
-                    subtitle="an app for early detection of Alzheimer's disease"
-                    label="flutter"
-                />
-                <ProjectCard
-                    img="https://i.imgur.com/4IHOCRG.png"
-                    title="michi"
-                    subtitle="an app for early detection of Alzheimer's disease"
-                    label="flutter"
-                />
+            <div className="flex flex-wrap -mx-1 overflow-hidden sm:-mx-1 md:-mx-1 lg:-mx-1 xl:-mx-1">
+                {projects.map(({ img, title, subtitle, label }) => (
+                    <div
+                        className="my-1 px-1 w-full overflow-hidden sm:my-1 sm:px-1 sm:w-1/2 md:my-1 md:px-1 md:w-full lg:my-1 lg:px-1 lg:w-1/4 xl:my-1 xl:px-1 xl:w-1/4"
+                        key={`${img}${title}${subtitle}${label}`}>
+                        <ProjectCard img={img} title={title} subtitle={subtitle} label={label} />
+                    </div>
+                ))}
             </div>
         </div>
     );
---
+++ src/data/project-data.js
@@ -0,0 +1,40 @@
+export const projects = [
+    {
+        img: 'https://i.imgur.com/vAIdzpI.jpg?1',
+        title: 'cognitionAD',
+        subtitle: "an app for early detection of Alzheimer's disease",
+        label: 'flutter'
+    },
+    {
+        img: 'https://i.imgur.com/Yx9CTjG.png',
+        title: 'niyamo',
+        subtitle: 'an app to overcome imposter syndrome',
+        label: 'flutter'
+    },
+
+    {
+        img: 'https://i.imgur.com/qSxz5sq.jpg',
+        title: 'aereor',
+        subtitle: 'a sustainability tracker for responsible global citizens',
+        label: 'flutter'
+    },
+    {
+        img: 'https://i.imgur.com/4IHOCRG.png',
+        title: 'fruti',
+        subtitle: 'an app for local farmers to sell their commodities directly to people',
+        label: 'flutter'
+    },
+    {
+        img: 'https://i.imgur.com/AeqIkvA.png',
+        title: 'poppity',
+        subtitle: 'a modern personal portfolio website',
+        label: 'flutter'
+    },
+
+    {
+        img: 'https://i.imgur.com/h2ZqKMW.png?1',
+        title: 'PCOSmopolitan',
+        subtitle: 'an ML powered test to detect potential PCOS patients',
+        label: 'flutter'
+    }
+];
