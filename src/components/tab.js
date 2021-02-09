e9b37af0c671b9768158adff6c6b2c194f2c2cd9
Vaishnavi <vaishdw2207@gmail.com>
2021-02-09 19:55:44 +0530

    feat: add classified skill section

--- src/components/tab.js
+++ src/components/tab.js
@@ -23,7 +23,7 @@ const Tabs = ({ color }) => {
                                 data-toggle="tab"
                                 href="#link1"
                                 role="tablist">
-                                <i className="fas fa-space-shuttle text-base mr-1"></i> Profile
+                                <i className="fas fa-space-shuttle text-base mr-1"></i> technical
                             </a>
                         </li>
                         <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
@@ -41,7 +41,7 @@ const Tabs = ({ color }) => {
                                 data-toggle="tab"
                                 href="#link2"
                                 role="tablist">
-                                <i className="fas fa-cog text-base mr-1"></i> Settings
+                                <i className="fas fa-cog text-base mr-1"></i> non-technical
                             </a>
                         </li>
                         <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
@@ -59,36 +59,21 @@ const Tabs = ({ color }) => {
                                 data-toggle="tab"
                                 href="#link3"
                                 role="tablist">
-                                <i className="fas fa-briefcase text-base mr-1"></i> Options
+                                <i className="fas fa-briefcase text-base mr-1"></i> misc
                             </a>
                         </li>
                     </ul>
                     <div className="relative flex flex-col min-w-0 break-words bg-purple-200 w-full mb-6 shadow-lg rounded">
                         <div className="px-4 py-5 flex-auto">
                             <div className="tab-content tab-space ">
-                                <div
-                                    className={openTab === 1 ? 'block' : 'hidden'}
-                                    id="link1"></div>
+                                <div className={openTab === 1 ? 'block' : 'hidden'} id="link1">
+                                    <p>work in progress</p>
+                                </div>
                                 <div className={openTab === 2 ? 'block' : 'hidden'} id="link2">
-                                    <p>
-                                        Completely synergize resource taxing relationships via
-                                        premier niche markets. Professionally cultivate one-to-one
-                                        customer service with robust ideas.
-                                        <br />
-                                        <br />
-                                        Dynamically innovate resource-leveling customer service for
-                                        state of the art customer service.
-                                    </p>
+                                    <p>work in progress</p>
                                 </div>
                                 <div className={openTab === 3 ? 'block' : 'hidden'} id="link3">
-                                    <p>
-                                        Efficiently unleash cross-media information without
-                                        cross-media value. Quickly maximize timely deliverables for
-                                        real-time schemas.
-                                        <br />
-                                        <br /> Dramatically maintain clicks-and-mortar solutions
-                                        without functional solutions.
-                                    </p>
+                                    <p>work in progress</p>
                                 </div>
                             </div>
                         </div>
