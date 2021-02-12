8562da89ca11c8eb09e1302edb3f5ff2f2a47d55
Vaishnavi <vaishdw2207@gmail.com>
2021-02-12 22:15:39 +0530

    fix: seo img fix

--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,7 @@ export default function Home() {
                     property="og:description"
                     content="Full Stack Developer | Data Engineer | Designer"
                 />
-                <meta
-                    property="og:image"
-                    content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-                />
+                <meta property="og:image" content="https://i.imgur.com/NMVIzOW.png" />
 
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="https://dwvicy.ml" />
@@ -30,10 +27,7 @@ export default function Home() {
                     property="twitter:description"
                     content="Full Stack Developer | Data Engineer | Designer"
                 />
-                <meta
-                    property="twitter:image"
-                    content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-                />
+                <meta property="twitter:image" content="https://i.imgur.com/NMVIzOW.png" />
             </Head>
 
             <div className="container m-auto w-10/2 sm:w-4/12 px-4 justify-center">
