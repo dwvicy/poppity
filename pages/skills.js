5f7a9bcc79f7dcc05ad31fb9c9010f970a645a86
Vaishnavi <vaishdw2207@gmail.com>
2021-02-13 11:41:26 +0530

    fix: skills fix

--- pages/skills.js
+++ pages/skills.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import TabsRender from '../src/components/tab';
+// import TabsRender from '../src/components/tab';
 
 export default function Skills() {
     return (
         <div className="flex flex-col min-h-screen">
-            <TabsRender />
+            <h1>Skills</h1>
+            <p>Work in progress</p>
+            {/* <TabsRender /> */}
         </div>
     );
 }
